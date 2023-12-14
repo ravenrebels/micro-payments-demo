521c0d5e04cdf20b3e183b09e8fa7721fdde2f95
Dick Larsson <dicklarsson@gmail.com>
2023-12-14 19:50:54 +0100

    Remove BIP-125 check, not active on Ravencoin

--- getReceived.js
+++ getReceived.js
@@ -24,33 +24,20 @@ export async function getReceived(address) {
   const mempool = await rpc("getaddressmempool", params);
   let received = b.received;
   for (const item of mempool) {
-    const transaction = await rpc("getrawtransaction", [item.txid, true]);
-    if (isRBFEnabled(transaction) === true) {
-      console.log("Skipping transaction", item.txid, " it's BRF enabled");
-      break;
-    }
-    if (item.assetName === "RVN" && item.satoshis > 0) {
-      received += item.satoshis;
-    }
+    try {
+      const mempoolEntry = await rpc("getmempoolentry", [item.txid]);
+      //Do not trust mempool entries that are dependent on other mempool entries.
+      if (mempoolEntry.depends.length > 0) {
+        continue;
+      }
+      if (item.assetName === "RVN" && item.satoshis > 0) {
+        received += item.satoshis;
+      }
+    } catch (e) {}
   }
   // Convert received from satoshis to Ravencoin: divide by 100 million since 1 ravencoin = 100 million satoshis
   if (received > 0) {
     return received / 100000000;
   }
   return received;
-}
-/*
- //a transaction can be flagged as RBF "Replace-By-Fee" if the sequence number of any of its inputs is less than 0xFFFFFFFF (4294967295).
- */
-function isRBFEnabled(transaction) {
-  if (!transaction || !transaction.vin) {
-    return false;
-  }
-
-  for (const input of transaction.vin) {
-    if (input.sequence < 0xffffffff) {
-      return true;
-    }
-  }
-  return false;
 }
